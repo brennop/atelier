b381af37edd516d2ee3cbbd3241a4d4395efa1ca
brennop <brennopcordeiro@gmail.com>
2023-02-11 17:32:15 -0300

    better way to run

---
+++ art/cube/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+  t.window.width = 756
+  t.window.height = 756
+end
+
---
+++ art/cube/main.lua
@@ -0,0 +1,137 @@
+package.path = package.path .. ";../?.lua"
+local renderer = require "lib.fragment"
+local save = require "lib.save"
+
+local _t = 0
+local size = 0.6
+
+function length(v)
+  return math.sqrt(v[1] * v[1] + v[2] * v[2] + v[3] * v[3])
+end
+
+function vecSum(a, b)
+  return { a[1] + b[1], a[2] + b[2], a[3] + b[3] }
+end
+
+function vecScale(a, b)
+  return {a[1] * b, a[2] * b, a[3] * b}
+end
+
+function clamp(v)
+  return math.max(0, math.min(1, v))
+end
+
+function dot(a, b)
+  return a[1] * b[1] + a[2] * b[2] + a[3] * b[3]
+end
+
+function abs(v)
+  return {math.abs(v[1]), math.abs(v[2]), math.abs(v[3])}
+end
+
+function max(a, b)
+  return {math.max(a[1], b), math.max(a[2], b), math.max(a[3], b)}
+end
+
+function rotX(v, a)
+  local c = math.cos(a)
+  local s = math.sin(a)
+  return { v[1], v[2] * c - v[3] * s, v[2] * s + v[3] * c }
+end
+
+function rotY(v, a)
+  local c = math.cos(a)
+  local s = math.sin(a)
+  return { v[1] * c + v[3] * s, v[2], -v[1] * s + v[3] * c }
+end
+
+function cube(q)
+  local p = vecSum(abs(q), {-size, -size, -size})
+  return length(max(p, 0)) - 0.07
+end
+
+function oct(q)
+  local p = abs(q)
+  return (p[1] + p[2] + p[3] - 1) * 0.5
+end
+
+function twist(v, l)
+  local k = math.sin(_t) * l
+  local c = math.cos(k * v[2])
+  local s = math.sin(k * v[2])
+  local a = {v[1] * c - v[3] * s, v[1] * s + v[3] * c, v[2]}
+  return a
+end
+
+function sdf(q)
+  local v = rotX(rotY(q, _t), _t)
+  -- local t = twist(v, 1)
+  return oct(v)
+end
+
+function raycast(ro, rd)
+  local a = 0
+
+  for i = 1, 64 do
+    local p = vecSum(ro, vecScale(rd, a))
+    local d = sdf(p)
+
+    if d < 0.0001 then
+      break
+    end
+
+    a = a + d
+  end
+
+  return a
+end
+
+function normalize(v)
+  local l = length(v)
+  if l == 0 then
+    return { 1000000, 1000000, 1000000 }
+  end
+  return {v[1] / l, v[2] / l, v[3] / l}
+end
+
+function normal(q)
+  local e = 0.0001
+  local n = {
+    sdf({q[1] + e, q[2], q[3]}) - sdf({q[1] - e, q[2], q[3]}),
+    sdf({q[1], q[2] + e, q[3]}) - sdf({q[1], q[2] - e, q[3]}),
+    sdf({q[1], q[2], q[3] + e}) - sdf({q[1], q[2], q[3] - e}),
+  }
+
+  return normalize(n)
+end
+
+local function art(x,y,t)
+  _t = t * 0.25
+
+  local ro = { 0, 0, 3 }
+  local rd = { (x - 0.5), (y - 0.5), -1 }
+
+  local p = raycast(ro, rd)
+
+  if p < 5 then
+    local q = vecSum(ro, vecScale(rd, p))
+    local n = normal(q)
+
+    local light = { 0, 0, 0.9 }
+    local diffuse = clamp(dot(n, light))
+
+    return 16 - diffuse * 3
+  end
+
+  return 16
+end
+
+function love.load()
+  save(function(f)
+    renderer(art, 64, 756, f * 0.2)
+  end, 24 * 10, "out")
+end
+
+function love.draw()
+  renderer(art, 64, 756, love.timer.getTime() * 5)
+end
