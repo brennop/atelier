a425d55b4de75da5ba10c4ef0734df9aa3a1d97f
brennop <brennopcordeiro@gmail.com>
2023-02-11 14:34:07 -0300

    recording

--- art/field/conf.lua
+++ art/field/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
-  t.window.width = 256
-  t.window.height = 256
+  t.window.width = 756
+  t.window.height = 756
   t.window.msaa = 2
 end
 
--- art/field/main.lua
+++ art/field/main.lua
@@ -1,4 +1,4 @@
-local k = 0.01
+local k = 0.007
 local t = 0
 
 function love.update(dt)
@@ -8,14 +8,14 @@ end
 function love.draw()
   love.graphics.clear(1, 1, 1, 1)
   love.graphics.setBlendMode("subtract")
-  for x = 32, 224, 4 do
-    for y = 32, 224, 4 do
+  for x = 32*3, 224*3, 4*3 do
+    for y = 32*3, 224*3, 4*3 do
       for i = 1, 3 do
-        local a = love.math.noise(x*k, y*k, t+i*0.05) * math.pi * 2
-        local color = { 0, 0, 0 }
+        a = love.math.noise(x*k,y*k,t+i*0.05)*math.pi*2
+        color = {0,0,0}
         color[i] = 1
         love.graphics.setColor(color)
-        love.graphics.circle("fill", x-math.sin(a)*8, y+math.cos(a)*8, a*0.4)
+        love.graphics.circle("fill", x-math.sin(a)*8, y+math.cos(a)*8, a)
       end
     end
   end
