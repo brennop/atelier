850ca2dc37e725e367656dd612df4370d7ccd909
brennop <brennopcordeiro@gmail.com>
2023-07-16 17:30:32 -0300

    rotating qr

--- art/qr.lua
+++ art/qr.lua
@@ -4,90 +4,58 @@ local noise_scale = 1.5
 
 local w = length * size
 local h = length * size
+local l = length * length
+
+local Vector = require "lib.vector"
 
 local colors = {
   {0.9, 0.0, 0.0},
   {0.0, 0.9, 0.0},
-  {0.0, 0.0, 0.9}
+  {0.0, 0.0, 0.9},
+  {0.9, 0.9, 0.9},
 }
 
-local finder_pattern = {
-  {1, 1, 1, 1, 1, 1, 1},
-  {1, 0, 0, 0, 0, 0, 1},
-  {1, 0, 1, 1, 1, 0, 1},
-  {1, 0, 1, 1, 1, 0, 1},
-  {1, 0, 1, 1, 1, 0, 1},
-  {1, 0, 0, 0, 0, 0, 1},
-  {1, 1, 1, 1, 1, 1, 1}
-}
+local vectors = {}
 
-local alignment_pattern = {
-  {1, 1, 1, 1, 1},
-  {1, 0, 0, 0, 1},
-  {1, 0, 1, 0, 1},
-  {1, 0, 0, 0, 1},
-  {1, 1, 1, 1, 1}
-}
+local function gen()
+  -- load qr-code.png
+  local img = love.image.newImageData("qr-code.png")
 
-local function deceive(def)
-  -- add spacing around finders
-  for i = 0, 7 do
-    for j = 0, 7 do
-      def[i][j] = 0
-      def[i][length - j - 1] = 0
-      def[length - i - 1][j] = 0
+  for c = 1, 3 do
+    for i = 0, length - 1 do
+      for j = 0, length - 1 do
+        if img:getPixel(i, j) == 0 then
+          local n = love.math.noise(i / noise_scale, j / noise_scale, c)
+          table.insert(vectors, { Vector(i, j, n * length), c })
+        end
+      end
     end
   end
+end
 
-  -- add finders
-  for i = 0, 6 do
-    for j = 0, 6 do
-      def[i][j] = finder_pattern[i + 1][j + 1]
-      def[i][length - j - 1] = finder_pattern[i + 1][j + 1]
-      def[length - i - 1][j] = finder_pattern[i + 1][j + 1]
-    end
-  end
-
-
-  -- add single alignment pattern
-  for i = 0, 4 do
-    for j = 0, 4 do
-      def[i + 16][j + 16] = alignment_pattern[i + 1][j + 1]
-    end
-  end
+function love.load()
+  gen()
 end
 
 function love.draw()
   love.graphics.clear(0.9, 0.9, 0.9)
   love.graphics.setBlendMode("subtract")
 
-  local t = love.timer.getTime() * 5
+  local t = love.timer.getTime()
 
   love.graphics.push()
   love.graphics.translate(love.graphics.getWidth() / 2 - w / 2, love.graphics.getHeight() / 2 - h / 2)
+  local center = Vector(w / 2, h / 2, h / 2)
 
-  for c = 1, 3 do
-    local def = {}
-    for i = 0, length - 1 do
-      def[i] = {}
-      for j = 0, length - 1 do
-        def[i][j] = 0
-      end
-    end
+  for i, k in ipairs(vectors) do
+    local v = k[1]
+    local a = math.cos(t * 0.5) * math.pi + math.pi
+    local _v = ((v * size) - center):rotatedY(a) + center
 
+    local c = colors[k[2]]
 
-    for i = 0, length - 1 do
-      for j = 0, length - 1 do
-        def[i][j] = love.math.noise(i / noise_scale, j / noise_scale, t / 2, c / 10) > 0.5 and 1 or 0
-
-        deceive(def)
-
-        if def[i][j] == 1 then
-          love.graphics.setColor(colors[c])
-          love.graphics.rectangle("fill", i * size, j * size, size, size)
-        end
-      end
-    end
+    love.graphics.setColor(c)
+    love.graphics.rectangle("fill", _v.x, _v.y, size, size)
   end
 
   love.graphics.pop()
