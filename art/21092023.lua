2c86bbf086d7ddc8a32f465332936110ed364aa2
brennop <brennopcordeiro@gmail.com>
2023-09-21 21:29:03 -0300

    2023-09-21

--- art/21092023.lua
+++ art/21092023.lua
@@ -7,19 +7,30 @@ local function sq(x) return x * x end
 
 local hy = require "lib.utils".length
 local fragment = require "lib.fragment"
+local save = require "lib.save"
+
+local function dot(a, b, c, d, e)
+  return a * b + c * d + e * c
+end
 
 local art = function(x, y, t)
   local n = love.math.noise(x*6, y*5, t/5) 
-  local k = n + x*.5 + t * .2
+  local k = n + x*.5 - t*.25
   local c = sqrt(48 - sq(hy({ x-.5, y-.5, 0 }) * 16)) * .1
+  c = dot(x - .5, cos(t * .25) * 2, sin(t * .25) * 2, c, y + .5)
 
-  local a = sin(max(tan(k + y - .5) * .25, 0)) * 1/c
+  local a = sin((cos(k + y - .5) * .25)) * 1/c
 
   -- return a * c * 4
-  return -abs(log(a) * c * 2)
+  -- return -abs(log(a) * c * 2)
+  -- return -sqrt(a)
+  return abs(log(a)) * .5
 end
 
 function love.load()
+  save(function(f)
+    fragment(art, 64, 768, f * 5)
+  end, 24 * 8, 24)
 end
 
 function love.draw()
